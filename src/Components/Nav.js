bdf4c09710e2b0ce52b9c8ed462ae1af4d40f36a
Scott Martínez <scottmtinez@gmail.com>
2025-04-03 19:25:47 -0500

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,27 +1,38 @@
-import './Nav.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css';
 import logo from '../images/Logo.png';
 
 function Nav() {
-  return (
-    <div className="nav-container">
-      <nav className="nav">
-        <div className="logo">
-        <Link to="/" className="navbar-logo">
-          <img src={logo} alt="Company Logo" className="Nav-logo" />
-        </Link>
-        </div>
-        <ul className="nav-links">
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/estimate">Estimate</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/account">Dashboard</Link></li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
+  // States
+    const [isOpen, setIsOpen] = useState(false);
+  
+    return (
+      <div className="nav-container">
+        <nav className="nav">
+          {/* Logo */}
+          <div className="logo">
+            <Link to="/" className="navbar-logo">
+              <img src={logo} alt="Company Logo" className="Nav-logo" />
+            </Link>
+          </div>
+  
+          {/* Mobile Menu Button */}
+          <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+            ☰
+          </button>
+  
+          {/* Navigation Links */}
+          <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+            <li><Link to="/home" onClick={() => setIsOpen(false)}>Home</Link></li>
+            <li><Link to="/services" onClick={() => setIsOpen(false)}>Services</Link></li>
+            <li><Link to="/about" onClick={() => setIsOpen(false)}>About Us</Link></li>
+            <li><Link to="/estimate" onClick={() => setIsOpen(false)}>Estimate</Link></li>
+            <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 
 export default Nav;
